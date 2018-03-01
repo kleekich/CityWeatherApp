6b1640191d3dcc563051acad40abfdc310988833
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-28 16:31:28 -0800

    Map view of each city correctly shows

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
 
 class GoogleMap extends Component {
 	componentDidMount() {
-		new.google.maps.Map(this.refs.map, {
+		new google.maps.Map(this.refs.map, {
 			zoom: 12,
 			center: {
 				lat: this.props.lat,
-				lng: this.props.lng
+				lng: this.props.lon
 			}
 		});
 	}
 
 	render() {
-		return <div ref="map" />;
+		return <div  className="mapsclass" ref="map" />;
 	}
 
 
 }
 
 export default GoogleMap;
+/*
+
+import React, { Component } from 'react';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+
+export default (props) => {
+	return (
+		<GoogleMapLoader
+			containerElement= { <div style={{height: '100%'}} /> }
+			googleMapElement= {
+				<GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
+			}
+		/>
+	);
+
+}
+*/
+

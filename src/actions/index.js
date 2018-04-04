4b155cede15f1589311b903e108181cf7a9c6fff
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-04-04 12:50:38 -0700

    change url for secure axios call with https

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ const API_KEY = '7ea8496c87abb60152df92467268d225';
 //const ROOT_URL = 'http://samples.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
 
 //ES6 Syntax
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
